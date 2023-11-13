00d7492c263fd469e1126e9dc2c5918be5a67b37
Jexy15 <jessica.rubino.94@gmail.com>
2023-11-13 21:14:13 +0100

    Creazione cartelle frontend

--- todo-list-angular/src/app/app.module.ts
+++ todo-list-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskInputComponent } from './task-input/task-input.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskInputComponent,
+    TaskListComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
